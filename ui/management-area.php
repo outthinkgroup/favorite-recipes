299c8fa126491173f60d48ec07ce8339afe72b7a
Joshua Kennedy <josh@outthinkgroup.com>
2020-07-27 13:34:19 -0400

    deleted old unused code

--- ui/management-area.php
+++ ui/management-area.php
@@ -1,24 +1,5 @@
 <?php
-function add_favorites_menu_item( $current_menu_items ) {
-  $current_menu_items = array_slice( $current_menu_items, 0, 4, true ) 
-  + array( 'my-favorites' => 'My Recipe Lists' )
-  + array_slice( $current_menu_items, 4, NULL, true );
-  return $current_menu_items;
 
-}
-//add_filter( 'woocommerce_account_menu_items', 'add_favorites_menu_item', 10, 1 );
-    
-
-/**
- * Add endpoint
- */
-function add_favorite_endpoint_to_members_area() {
-  add_rewrite_endpoint( 'my-favorites', EP_PAGES );
-}
-add_action( 'init', 'add_favorite_endpoint_to_members_area' );
-
-
-//add_action( 'woocommerce_account_my-favorites_endpoint', 'my_favorites_endpoint_content');
 
 add_shortcode( 'myfavorites', 'my_favorites_endpoint_content' );
 
